dad1fc54103167658426df0d309859ccfd82f0a9
Maksim Grigoryev <maks.samoylov.83@mail.ru>
2025-02-05 09:57:59 +0300

    add function 'mask_account_card'

---
+++ src/wiget.py
@@ -0,0 +1,14 @@
+import re
+from masks import get_mask_card_number
+from masks import get_mask_account
+
+
+def mask_account_card(card_number: str) -> str:
+    """Функция маскировки как банковского счета, так и номера карты"""
+    only_numbers = re.findall(r'\d+', card_number)
+    str_only_numbers = ''.join(only_numbers)
+    if 'Счет' in card_number:
+        return get_mask_account(str_only_numbers)
+    else:
+        return get_mask_card_number(str_only_numbers)
+
