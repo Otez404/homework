ffb5cb605838f5a2c39c113e643086ebdd26844a
Maksim Grigoryev <maks.samoylov.83@mail.ru>
2025-02-05 08:35:17 +0300

    iznachalnoe polojenie

---
+++ src/masks.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+
+def get_mask_card_number(card_number: Union[str, int]) -> str:
+    """Функция маскировки номера банковской карты"""
+    str_card_number = str(card_number)
+    if len(str_card_number) != 16:
+        return "Введен неверный номер карты"
+    else:
+        return f"{str_card_number[:4]} {str_card_number[4:6]}** **** {str_card_number[-4:]}"
+
+
+def get_mask_account(account_number: Union[str, int]) -> str:
+    """Функция маскировки номера банковского счета"""
+    str_account_number = str(account_number)
+    if len(str_account_number) != 20:
+        return "Введен неверный номер счета"
+    else:
+        return f"**{str_account_number[-4:]}"
