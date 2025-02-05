3345e891c210b9f8720114ae8c0445e42431eff2
Maksim Grigoryev <maks.samoylov.83@mail.ru>
2025-02-05 11:41:38 +0300

    changing for PEP8 standart

--- src/wiget.py
+++ src/wiget.py
@@ -1,6 +1,8 @@
+import datetime as dt
 import re
-from masks import get_mask_card_number
+
 from masks import get_mask_account
+from masks import get_mask_card_number
 
 
 def mask_account_card(card_number: str) -> str:
@@ -15,3 +17,6 @@ def mask_account_card(card_number: str) -> str:
 
 def get_date(date_format: str) -> str:
     """Функция изменения формата даты"""
+    date_object = dt.datetime.strptime(date_format[0:10], '%Y-%m-%d')
+    date_class_str = date_object.strftime('%d.%m.%Y')
+    return date_class_str
